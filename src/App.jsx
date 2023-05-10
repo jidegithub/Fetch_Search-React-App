d6c7f4701906f9412079e66200b03b640dc52029
olajide.o <olutolajide@gmail.com>
2023-05-10 13:08:21 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Posts from './components/Posts'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
@@ -18,18 +15,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Posts/>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="card" >
+        <Button/>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div> */}
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      </div> 
     </div>
   )
 }
